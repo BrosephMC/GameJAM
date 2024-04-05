2ef9c8a1076ef723f64cd08c57c3a58dbb6d8462
BrosephMC <jojoglick418@gmail.com>
2024-04-05 18:32:46 -0500

    better

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Simple Platformer")
+pygame.display.set_caption("Genshin Impact Lite")
 
 # Set colors
 WHITE = (255, 255, 255)
@@ -17,17 +17,9 @@ BLUE = (0, 0, 255)
 # Set player attributes
 player_width = 50
 player_height = 50
-player_x = 50
-player_y = SCREEN_HEIGHT - player_height - 50
+player_x = SCREEN_WIDTH // 2 - player_width // 2
+player_y = SCREEN_HEIGHT // 2 - player_height // 2
 player_vel = 5
-
-# Set gravity
-gravity = 0.5
-player_jump = False
-jump_velocity = 10
-
-# Set floor attributes
-floor_y = SCREEN_HEIGHT - 50
 
 # Main game loop
 running = True
@@ -36,33 +28,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and not player_jump:
-                player_jump = True
-
     # Player movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         player_x -= player_vel
     if keys[pygame.K_RIGHT]:
         player_x += player_vel
+    if keys[pygame.K_UP]:
+        player_y -= player_vel
+    if keys[pygame.K_DOWN]:
+        player_y += player_vel
 
-    # Jumping mechanic
-    if player_jump:
-        player_y -= jump_velocity
-        jump_velocity -= gravity
-        if jump_velocity < -10:
-            player_jump = False
-            jump_velocity = 10
-
-    # Collision detection with the floor
-    if player_y >= floor_y - player_height:
-        player_y = floor_y - player_height
-        player_jump = False
-        jump_velocity = 10
-
-    # Apply gravity
-    player_y += gravity
+    # Boundary checking
+    player_x = max(0, min(SCREEN_WIDTH - player_width, player_x))
+    player_y = max(0, min(SCREEN_HEIGHT - player_height, player_y))
 
     # Draw everything
     screen.fill(WHITE)
